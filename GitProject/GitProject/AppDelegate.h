e8b58f8e9f6ba3482adec80e1c761b245312b638
leil <leil161@kotei-info.com>
2018-04-04 13:39:18 +0800

    上传项目

---
+++ GitProject/GitProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitProject
+//
+//  Created by liulei on 2018/4/4.
+//  Copyright © 2018年 KOTEI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitProject/GitProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitProject
+//
+//  Created by liulei on 2018/4/4.
+//  Copyright © 2018年 KOTEI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
