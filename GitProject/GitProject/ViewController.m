f96ed4a85587d1e4f5fa674903795bf763d14dfb
leil <leil161@kotei-info.com>
2018-04-04 16:24:23 +0800

    002
    提交

--- GitProject/GitProject/ViewController.m
+++ GitProject/GitProject/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+// "Sources/ImageSource"
 @interface ViewController ()
 
 @end
@@ -16,6 +16,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    [self.view setBackgroundColor:[UIColor redColor]];
+    UIImageView* imageV = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"cat1"]];
+//    imageV.image = [UIImage imageNamed:@"1"];
+    imageV.frame = CGRectMake(20, 64, 30, 30);
+    imageV.backgroundColor = [UIColor redColor];
+    [self.view addSubview:imageV];
+
+
+    NSString* path =   [[NSBundle mainBundle] bundlePath];
+    NSString *imagePath = [[NSBundle mainBundle] pathForResource:
+                           @"IMG_01" ofType:@"jpg"];
+    UIImage *appleImage = [[UIImage alloc] initWithContentsOfFile:imagePath];
+    imageV.image = appleImage;
+    NSLog(@"path = %@",path);
+
+    NSArray *allBun = [NSBundle allBundles];
+    NSLog(@"path = %@",allBun);
+
+//    //inDirectory参数可有可无
+//    NSString *earth = [[NSBundle mainBundle] pathForResource:@"IMG_01" ofType:@"jpg" inDirectory:@"ImageSource/image"];
+//    NSLog(@"path = %@",earth);
+
+//可行
+
+//    NSString* path1 =  [[NSBundle mainBundle] pathForResource:@"ImageSource/images/IMG_01" ofType:@"jpg"];
+//    NSLog(@"path = %@",path1);
+
+
+    //可行
+    NSString* resourceBundle =  [[NSBundle mainBundle] pathForResource:@"ImageSource/Settings" ofType:@"bundle"];
+     NSString *earth = [[NSBundle bundleWithPath:resourceBundle] pathForResource:@"IMG_01"ofType:@"jpg"];
+
+    appleImage = [[UIImage alloc] initWithContentsOfFile:earth];
+    imageV.image = appleImage;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
